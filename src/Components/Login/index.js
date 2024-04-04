77180100521d3f966c8190616ecbbe2dfefc3099
SagnikGhosh18 <sagnikghosh18@gmail.com>
2024-04-05 02:32:37 +0530

    implemented basic login functionalities

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -1,39 +1,73 @@
 import React from 'react'
-import { useState } from 'react';
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import { Link, redirect } from 'react-router-dom';
 
 
 export const Login = () => {
   const [email, setEmail] = useState('');
   const [pass, setPass] = useState('');
+  const [users, setUsers] = useState([]);
+  const [page, setPage] = useState('products');
+
+  async function getUsers() {
+    try {
+      const response = await fetch('http://localhost:5000/users'); // Ensure correct protocol
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      setUsers(data);
+    } catch (error) {
+      console.error("Failed to fetch products:", error);
+    }
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+  const emailhandler = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const passhandler = (e) => {
+    setPass(e.target.value);
+  }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(email);
+    // e.preventDefault();
+    const valid = users.filter(user=>(user.email===email && user.pass===pass));
+    console.log(valid);
+    if(valid.length===1)
+      setPage('products');
+    else{
+      setPage('');
+    }
   }
   return (
     <>
-      <div class="flex items-center justify-center min-h-screen">
-        <div class="w-full max-w-xs m-4">
-          <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-            <div class="mb-4">
-              <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="w-full max-w-xs m-4">
+          <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
                 Email Address
               </label>
-              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" />
+              <input value={email} onChange={emailhandler} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" />
             </div>
-            <div class="mb-6">
-              <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+            <div className="mb-6">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                 Password
               </label>
-              <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******" />
+              <input value={pass} onChange={passhandler} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******" />
             </div>
-            <div class="flex items-center justify-between">
-              <Link to="/products">
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+            <div className="flex items-center justify-between">
+              <Link to={`/${page}`}>
+                <button onClick={handleSubmit} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
                   Sign In
-                </button>
+                </button>  
               </Link>
+              
             </div>
           </form>
         </div>
